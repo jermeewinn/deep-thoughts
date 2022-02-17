e82fd9d7af07582cbc5b4703e14f925fc8ac278a
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-17 00:14:09 -0800

    added functionality for JWT and additional mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,15 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        //All thoughts by username
+        //Get all thoughts by username
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
         },
-        //Single thought by id
+        //Get single thought by id
         thought: async (parent, { _id }) => {
             return Thought.findOne({ _id });
         },
@@ -24,6 +26,30 @@ const resolvers = {
                 .select('-__v -password')
                 .populate('friends')
                 .populate('thoughts');
+        }
+    },
+    Mutation: {
+        //Create new user
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        //Log user in
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+
+            return { token, user };
         }
     }
 };
